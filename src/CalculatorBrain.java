4b354fb173f4f208e6d5db12196767fbe930b3f9
DanHT22 <tolumes.daniela@gmail.com>
2023-07-31 11:07:08 -0600

    inicializando repositorio

---
+++ src/CalculatorBrain.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalculatorBrain {
+
+	public static void main(String[] args) {
+		 {
+		        Scanner console = new Scanner( System.in );
+		        
+		        System.out.println( "Enter your full name:  " );
+		        String name = console.nextLine();
+		        printNameCharacters( name );
+		        printNameLength( '' );
+		        console.close();
+		    }
+		 private static void  printNameLength (String name);
+	}
+		 private static void  printNameCharacters (String name);
+	}
+
+}
