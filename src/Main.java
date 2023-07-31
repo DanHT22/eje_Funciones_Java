37cffa48c46d67e9bb783bc03da6433c07870e4d
DanHT22 <tolumes.daniela@gmail.com>
2023-07-31 12:54:06 -0600

    ajustes

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
-public class Main{
-	
-    	   public static void main(String[] args) {
-    	        Scanner scanner = new Scanner(System.in);
-    	        System.out.println("ingresa dos numeros : ");
-    	        int a = scanner.nextInt();
-    	        int b = scanner.nextInt();
+public class Main
+{
+    public static void main( String[] args )
+    {
+        Scanner console = new Scanner( System.in );
+        System.out.println( "Enter your full name:  " );
+        String name = console.next();
+        printNameCharacters( name );
+        printNameLength( name );
+        console.close();
+    }
 
-    	        System.out.println("la suma es: " + sum(a, b));
-    	        System.out.println("la resta es: " + rest(a, b));
-    	        System.out.println("la multiplicacion es: " + multi(a, b));
-    	        System.out.println("la division es: " + divi(a, b));
+    private static void printNameLength( String name )
+    {
+    	int numL = name.length();
+    	System.out.println("El numero de letras es : " + numL);
+    }
 
-    	    }
-    	    private static int sum (int a, int b) {
-    	        return (a + b);
-    	    }
-    	    private static int rest (int a, int b) {
-    	        return (a - b);
-    	    }
-    	    private static int multi (int a, int b) {
-    	        return (a * b);
-    	    }
-    	    private static double divi (double a, double b) {
-    	        return (a / b);
-    	    }
-    	}
-    
+    private static void printNameCharacters( String name )
+    {
+       for (int cont = 0; cont < name.length(); cont++) {
+        	System.out.println(name.charAt(cont));
+        	
+        }
+    }
+
+
+}
